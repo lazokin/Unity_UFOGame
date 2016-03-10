67eb058d7d43da691644ca382e7d20728dd43a03
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-10 22:09:09 +1100

    Count collectables and display score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody2D rb2d;
+    private int count;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
     }
 
     void FixedUpdate()
@@ -24,6 +32,17 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
